c3c5931c71fc511ceccf2aedf5f99e183ae7bfdb
sammiearchie77 <chinonsosamuel29@gmail.com>
2023-07-06 14:52:18 +0100

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines a function that determines if a box containing a list
+   of lists can be opened using keys stored in the lists
+"""
+def canUnlockAll(boxes):
+    keys = set()
+    for sublist in boxes:
+        keys.update(sublist)
+    return len(keys) == len(boxex)
