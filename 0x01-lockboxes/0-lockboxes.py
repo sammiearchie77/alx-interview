82fdd03309cf00f60d3a46485e5bc96609e81b3c
sammiearchie77 <chinonsosamuel29@gmail.com>
2023-07-06 15:02:52 +0100

    Refactored code

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python3
-"""Defines a function that determines if a box containing a list
-   of lists can be opened using keys stored in the lists
 """
+    A method that determines if all the boxes can be opened.
+        - Prototype: def canUnlockAll(boxes)
+        - boxes is a list of lists
+        - Return True if all boxes can be opened, else return False
+"""
+
 def canUnlockAll(boxes):
-    keys = set()
-    for sublist in boxes:
-        keys.update(sublist)
-    return len(keys) == len(boxes)
+    n = len(boxes)
+    unlocked = [False] * n
+    unlocked[0] = True  # The first box is initially unlocked
+
+    # Use a stack to keep track of boxes to explore
+    stack = [0]
+
+    while stack:
+        box = stack.pop()
+
+        # Iterate through the keys in the current box
+        for key in boxes[box]:
+            if key < n and not unlocked[key]:
+                unlocked[key] = True
+                stack.append(key)
+
+    # Check if all boxes are unlocked
+    return all(unlocked)
+
